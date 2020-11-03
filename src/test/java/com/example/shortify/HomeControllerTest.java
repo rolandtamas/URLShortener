1ac2463ab07ebab337dbb9f4747b78ec612abb59
Roland Tamas <rolandtamas98@gmail.com>
2020-11-03 12:42:47 +0200

    added the HtmlUnit dependency, and wrote a few test cases using mockmvc and HtmlUnit

--- src/test/java/com/example/shortify/HomeControllerTest.java
+++ src/test/java/com/example/shortify/HomeControllerTest.java
@@ -1,36 +1,67 @@
 package com.example.shortify;
 
-import com.example.shortify.controllers.HomeController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+//I used an api called HTMLUnit to test things such as the title of the page
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class HomeControllerTest {
 
-    private static HomeController homeController;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @BeforeAll
-    public static void setUp(){
-        homeController = new HomeController();
+    private final WebClient webClient = new WebClient();
+
+
+    @DisplayName("Test to check if header text is present in page")
+    @Test //tests if we have a welcome message contained in the homepage
+    public void shouldReturnHeaderMessage() throws Exception{
+        this.mockMvc.perform(get("/"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(containsString("Welcome to Shortify")));
     }
 
-    @Test //this one succeeds
-    public void frontPageIsLoaded() //This test checks if the front page was loaded
-    {
-        String pageName = homeController.loadHomepage();
-        assertEquals("index",pageName);
-    }
-
+    @DisplayName("Test to check if page title is ok, using HtmlUnit")
     @Test
-    public void noPageGetsLoaded() //this one fails
-    {
-        String pageName = homeController.loadHomepage();
-        assertEquals("home",pageName);
+    public void whenOpeningPage_thenTitleIsOK() throws Exception{
+        HtmlPage frontpage = webClient.getPage("http://localhost:8080");
+        assertEquals("Shortify - Shorten URLs and save time",frontpage.getTitleText());
     }
 
+    @DisplayName("Test to check if 2 input fields are present")
+    @Test
+    public void whenOpeningPage_twoInputFieldsExist() throws Exception {
+        HtmlPage frontpage = webClient.getPage("http://localhost:8080");
+        String fullURLXpath="/html/body/div/form/div[1]/input";
+        String shortURLXpath = "/html/body/div/form/div[2]/input";
+
+        HtmlInput fullURLInput = (HtmlInput) frontpage.getByXPath(fullURLXpath).get(0);
+        HtmlInput shortURLInput = (HtmlInput) frontpage.getByXPath(shortURLXpath).get(0);
+
+        assertNotNull(fullURLInput);
+        assertNotNull(shortURLInput);
+
+    }
 
 }
